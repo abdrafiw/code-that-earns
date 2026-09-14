47ae3c1a1ac3744057d5a81a650760b26f4aa29a
rafiw <rafiw@users-MacBook-Pro.local>
2026-09-14 20:56:29 +0000

    style: normalize mobile navigation spacing

--- src/layout/header/components/MobileNavigationSheet.tsx
+++ src/layout/header/components/MobileNavigationSheet.tsx
@@ -11,7 +11,9 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from '../../../components/ui/alert-dialog';
+
 import { Button } from '../../../components/ui/button';
+
 import {
   Sheet,
   SheetContent,
